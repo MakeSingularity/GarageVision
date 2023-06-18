66b8e6e210a9515cffb221a603092376a1e9841e
MakeSingularity <makesingularity@gmail.com>
2023-06-18 10:10:30 -0700

    first possible working firmware
    After refining the code with ChatGTP this is the first time the code came back with no errors.

--- ESP32CAM/.ipynb_checkpoints/README-checkpoint.md
+++ ESP32CAM/.ipynb_checkpoints/README-checkpoint.md
@@ -75,3 +75,131 @@ void loop() {
   // Read the first line of the request
   String req
 ```
+## Dev Notes
+As we can see ChatGTP had a problem with the complexity of our question. So we asked it to recalculate the same question. This will generate a continued response built off of the first prompt. Sometimes this works sometimes you have to refine the prompt. 
+
+## ChatGTPPrompt
+Create C++ code for the ESP32-CAM AI-THINKER to output livestream camera to path and include an OTA updater webpage.
+
+## ChatGTPResponse
+Here's an example of C++ code that sets up the ESP32-CAM AI-THINKER to output
+
+a livestream of the camera to a specific path and includes an OTA updater webpage:
+```C++
+#include <WiFi.h>
+#include <esp_camera.h>
+#include <esp_http_server.h>
+
+const char* ssid = "your_ssid";
+const char* password = "your_password";
+const char* stream_path = "/stream";
+const char* update_path = "/update";
+
+httpd_handle_t server = NULL;
+
+void setup() {
+  // Initialize the camera
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Start the camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.println("Error initializing camera");
+    return;
+  }
+  Serial.println("Camera initialized");
+
+  // Start the server
+  httpd_config_t http_config = HTTPD_DEFAULT_CONFIG();
+  http_config.uri_match_fn = httpd_uri_match_wildcard;
+  err = httpd_start(&server, &http_config);
+  if (err != ESP_OK) {
+    Serial.println("Error starting server");
+    return;
+  }
+  Serial.println("Server started");
+
+  // Register the stream endpoint
+  httpd_uri_t stream_uri = {
+    .uri = stream_path,
+    .method = HTTP_GET,
+    .handler = stream_handler,
+    .user
+
+_ctx = NULL
+  };
+  httpd_register_uri_handler(server, &stream_uri);
+
+  // Register the OTA update endpoint
+  httpd_uri_t update_uri = {
+    .uri = update_path,
+    .method = HTTP_POST,
+    .handler = update_handler,
+    .user_ctx = NULL
+  };
+  httpd_register_uri_handler(server, &update_uri);
+}
+
+void loop() {
+  // Handle client requests
+  httpd_handle_t client;
+  while (client = httpd_accept(server, 0)) {
+    httpd_resp_send_404(client);
+    httpd_resp_close(client);
+  }
+}
+
+esp_err_t stream_handler(httpd_req_t* req) {
+  camera_fb_t* fb = esp_camera_fb_get();
+  if (!fb) {
+    httpd_resp_send_500(req);
+    return ESP_FAIL;
+  }
+
+  httpd_resp_set_type(req, "image/jpeg");
+  httpd_resp_send(req, (const char*)fb->buf, fb->len);
+
+  esp_camera_fb_return(fb);
+  return ESP_OK;
+}
+
+esp_err_t update_handler(httpd_req_t* req) {
+  // Handle OTA update here
+  // ...
+  return ESP_OK;
+}
+```
+
+Note that this is a basic example and may require further modifications and additions depending on your specific requirements and the libraries you are using. It assumes you have already installed the necessary libraries (such as `WiFi` and `esp_camera`) and have the appropriate hardware connections set up.
+
+Make sure to customize the `ssid` and `password` variables with your WiFi credentials, and adjust the `stream_path` and `update_path` to the desired endpoints for the livestream and OTA update respectively.
+
+Remember to consult the documentation and examples provided by the libraries you are using for more information on specific configurations and functionalities.
--- ESP32CAM/MasterCam_firmware/MasterCam_firmware.ino
+++ ESP32CAM/MasterCam_firmware/MasterCam_firmware.ino
@@ -1,19 +1,147 @@
-#include "esp_camera.h"
-#include <Arduino.h>
 #include <WiFi.h>
-#include <AsyncTCP.h>
+//#include <esp_http_server.h>
 #include <ESPAsyncWebServer.h>
-#include <iostream>
-#include <sstream>
-#include <ESP32Servo.h>
+#include "esp_camera.h"
+//#include "esp_timer.h"
+//#include "img_converters.h"
+#include <Update.h>
 
-#define DUMMY_SERVO1_PIN 12 //We need to create 2 dummy servos.
-#define DUMMY_SERVO2_PIN 13 //So that ESP32Servo library does not interfere with pwm channel and timer used by esp32 camera.
+const char* ssid = "ProjectSunday";
+const char* password = "D34D8YT3";
+const char* stream_path = "/stream";
+const char* update_path = "/update";
 
-#define PAN_PIN 14
-#define TILT_PIN 15
+AsyncWebServer server(80);
 
-Servo dummyServo1;
-Servo dummyServo2;
-Servo panServo;
-Servo tiltServo;
+//httpd_handle_t server = NULL;
+
+//esp_err_t update_handler(httpd_req_t* req) {
+//  //Handle OTA update here
+//  /// ...
+//  return ESP_OK;
+//}
+
+//esp_err_t stream_handler(httpd_req_t* req) {
+//  camera_fb_t* fb = NULL;
+//  esp_err_t res = ESP_OK;
+//  
+//  fb = esp_camera_fb_get();
+//}
+  
+void setup() {
+  // Initialize Serial monitor
+  Serial.begin(115200);
+  while (!Serial);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Initialize camera
+  camera_config_t config;
+  // Set camera configuration as per your specific board
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = 5; //Y2_GPIO_NUM;
+  config.pin_d1 = 18; //Y3_GPIO_NUM;
+  config.pin_d2 = 19; //Y4_GPIO_NUM;
+  config.pin_d3 = 21; //Y5_GPIO_NUM;
+  config.pin_d4 = 36; //Y6_GPIO_NUM;
+  config.pin_d5 = 39; //Y7_GPIO_NUM;
+  config.pin_d6 = 34; //Y8_GPIO_NUM;
+  config.pin_d7 = 35; //Y9_GPIO_NUM;
+  config.pin_xclk = 0; //XCLK_GPIO_NUM;
+  config.pin_pclk = 22; //PCLK_GPIO_NUM;
+  config.pin_vsync = 25; //VSYNC_GPIO_NUM;
+  config.pin_href = 23; //HREF_GPIO_NUM;
+  config.pin_sscb_sda = 26; //SIOD_GPIO_NUM;
+  config.pin_sscb_scl = 27; //SIOC_GPIO_NUM;
+  config.pin_pwdn = 32; //PWDN_GPIO_NUM;
+  config.pin_reset = -1; //RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.println("Camera initialization failed");
+    while (1);
+  }
+  Serial.println("Camera initialized");
+
+  // Start the server
+  server.on(stream_path, HTTP_GET, [](AsyncWebServerRequest* request){
+    camera_fb_t* fb = esp_camera_fb_get();
+    if (!fb) {
+      request->send(500, "text/plain", "Camera capture failed");
+    } else {
+      request->send_P(200, "image/jpeg", fb->buf, fb->len);
+      esp_camera_fb_return(fb);
+    }
+  });
+
+  server.on(update_path, HTTP_POST, [](AsyncWebServerRequest* request){
+    AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", (Update.hasError()) ? "Update failed" : "Update successful");
+    response->addHeader("Connection", "close");
+    request->send(response);
+  }, [](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
+    if (!index) {
+      Serial.println("Updating firmware...");
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)){
+        Update.printError(Serial);
+      }
+    }
+    if(Update.write(data, len) != len) {
+      Update.printError(Serial);
+    }
+    if (final) {
+      if (Update.end(true)) {
+        Serial.println("Firmware update successfully, Rebooting...");
+        ESP.restart();
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+
+  server.begin();
+}
+  //httpd_config_t http_config = HTTPD_DEFAULT_CONFIG();
+  //http_config.uri_match_fn = httpd_uri_match_wildcard;
+  //err = httpd_start(&server, &http_config);
+  //if (err != ESP_OK) {
+  //  Serial.println("Error starting server");
+  //  while (1) {}
+  //}
+  //Serial.println("Server started");
+
+  // Register the stream endpoint
+//  httpd_uri_t stream_uri = {
+//    .uri = stream_path,
+//    .method = HTTP_GET,
+//    .handler = stream_handler,
+//    .user_ctx = NULL
+//  };
+//  httpd_register_uri_handler(server, &stream_uri);
+
+  // Register the OTA update endpoint
+//  httpd_uri_t update_uri = {
+//    .uri = update_path,
+//    .method = HTTP_POST,
+//    .handler = update_handler,
+//    .user_ctx = NULL
+//  };
+//  httpd_register_uri_handler(server, &update_uri);
+//}
+
+void loop() {
+  // Handle client requests
+  //httpd_handle_t client;
+  //while ((client = httpd_accept(server, 0)) != NULL) {
+  //  httpd_resp_send_404(client, "Not Found");
+ //   httpd_resp_close(client);
+ // }
+}
